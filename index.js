ee37339035ea9ef454a53537320bd4beb5f0af2f
kiyo <kiyossy040715@gmail.com>
2020-01-15 16:30:56 +0900

    バグ修正

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ const fragmentShaders = [  // Define some simple shaders to apply to the camera 
       vec4 c = texture2D(sampler, texUv);
       vec3 keyColor = vec3(.99, .498, .313);
       float d = distance(keyColor,c.rgb);
-      vec3 rgb = (d - 0.99 < 0) ? vec3(.1, .1, .1) : c.rgb;
+      vec3 rgb = (d - 1.9 < 0) ? vec3(.1, .1, .1) : c.rgb;
       gl_FragColor = vec4(rgb, c.a);
     }`,
 ]
