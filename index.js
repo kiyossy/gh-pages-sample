874a1d4293b128bb34f8e41d7ede083d51306fe9
kiyo <kiyossy040715@gmail.com>
2020-01-15 15:48:54 +0900

    shader変更処理

--- index.js
+++ index.js
@@ -1,132 +1,115 @@
-let c, gl;
-let aLoc = [];
-let uLoc = [];
-let mx = 0.5, my = 0.5;
+// Copyright (c) 2018 8th Wall, Inc.
 
-function initWebGL() {
-    c = document.getElementById("c");
-    gl = c.getContext("webgl");
-    resizeCanvas();
-    window.addEventListener("resize", function(){
-        resizeCanvas();
-    });
-    c.addEventListener('mousemove', mouseMove, true);
+const fragmentShaders = [  // Define some simple shaders to apply to the camera feed.
+  ` precision mediump float;  // Just the camera feed.
+    varying vec2 texUv;
+    uniform sampler2D sampler;
+    void main() { gl_FragColor = texture2D(sampler, texUv); }`,
+  ` precision mediump float;  // Color boost.
+    varying vec2 texUv;
+    uniform sampler2D sampler;
+    void main() {
+      vec4 c = texture2D(sampler, texUv);
+      float y = dot(c.rgb, vec3(0.299, 0.587, 0.114));
+      float u = dot(c.rgb, vec3(-.159, -.331, .5)) * 6.0;
+      float v = dot(c.rgb, vec3(.5, -.419, -.081)) * 3.0;
+      gl_FragColor = vec4(y + 1.4 * v, y - .343 * u - .711 * v, y + 1.765 * u, c.a);
+    }`,
+  ` precision mediump float;  // Vignette.
+    varying vec2 texUv;
+    uniform sampler2D sampler;
+    void main() {
+      float x = texUv.x - .5;
+      float y = texUv.y - .5;
+      float v = 1.5 - sqrt(x * x + y * y) * 2.5;
+      vec4 c = texture2D(sampler, texUv);
+      gl_FragColor = vec4(c.rgb * (v > 1.0 ? 1.0 : v), c.a);
+    }`,
+  ` precision mediump float;  // Black and white.
+    varying vec2 texUv;
+    uniform sampler2D sampler;
+    void main() {
+      vec4 c = texture2D(sampler, texUv);
+      gl_FragColor = vec4(vec3(dot(c.rgb, vec3(0.299, 0.587, 0.114))), c.a);
+    }`,
+  ` precision mediump float;  // Sepia.
+    varying vec2 texUv;
+    uniform sampler2D sampler;
+    void main() {
+      vec4 c = texture2D(sampler, texUv);
+      gl_FragColor.r = dot(c.rgb, vec3(.393, .769, .189));
+      gl_FragColor.g = dot(c.rgb, vec3(.349, .686, .168));
+      gl_FragColor.b = dot(c.rgb, vec3(.272, .534, .131));
+      gl_FragColor.a = c.a;
+    }`,
+  ` precision mediump float;  // Purple.
+    varying vec2 texUv;
+    uniform sampler2D sampler;
+    void main() {
+      vec4 c = texture2D(sampler, texUv);
+      float y = dot(c.rgb, vec3(0.299, 0.587, 0.114));
+      vec3 p = vec3(.463, .067, .712);
+      vec3 rgb = y < .25 ? (y * 4.0) * p : ((y - .25) * 1.333) * (vec3(1.0, 1.0, 1.0) - p) + p;
+      gl_FragColor = vec4(rgb, c.a);
+    }`,
+]
+
+// Define a custom pipeline module. This module cycles through a set of pre-defined shaders each
+// time the next button is pressed. It also updates the button style on orientation changes.
+const nextbuttonPipelineModule = () => {
+  const nextButton = document.getElementById('nextbutton')
+  let idx = 0  // Index of the shader to use next.
+
+  const nextShader = () => {
+    // Reconfigure the texture renderer pipline module to use the next shader.
+    XR8.GlTextureRenderer.configure({fragmentSource: fragmentShaders[idx]})
+    idx = (idx + 1) % fragmentShaders.length
+  }
+
+  nextShader()                     // Call 'nextShader' once to set the first shader.
+  nextButton.onclick = nextShader  // Switch to the next shader when the next button is pressed.
+
+  const adjustButtonTextCenter = ({orientation}) => { // Update the line height on the button.
+    const ww = window.innerWidth
+    const wh = window.innerHeight
+
+    // Wait for orientation change to take effect before handling resize.
+    if (((orientation == 0 || orientation == 180) && ww > wh)
+      || ((orientation == 90 || orientation == -90) && wh > ww)) {
+      window.requestAnimationFrame(() => adjustButtonTextCenter({orientation}))
+      return
+    }
+
+    nextButton.style.lineHeight = `${nextButton.getBoundingClientRect().height}px`
+  }
+
+  // Return a pipeline module that updates the state of the UI on relevant lifecycle events.
+  return {
+    name: 'nextbutton',
+    onStart: ({orientation}) => {
+      nextButton.style.visibility = 'visible'
+      adjustButtonTextCenter({orientation})
+    },
+    onDeviceOrientationChange: ({orientation}) => { adjustButtonTextCenter({orientation}) },
+  }
 }
 
-function resizeCanvas() {
-    c.width = window.innerWidth;
-    c.height = window.innerHeight;
-    gl.viewport(0, 0, c.width, c.height);
+const onxrloaded = () => {
+  XR8.addCameraPipelineModules([  // Add camera pipeline modules.
+    // Existing pipeline modules.
+    XR8.GlTextureRenderer.pipelineModule(),      // Draws the camera feed.
+    XRExtras.AlmostThere.pipelineModule(),       // Detects unsupported browsers and gives hints.
+    XRExtras.FullWindowCanvas.pipelineModule(),  // Modifies the canvas to fill the window.
+    XRExtras.Loading.pipelineModule(),           // Manages the loading screen on startup.
+    XRExtras.RuntimeError.pipelineModule(),      // Shows an error image on runtime error.
+    // Custom pipeline modules.
+    nextbuttonPipelineModule(),             // Cycles through shaders and keeps UI up to date.
+  ])
+
+  // Request camera permissions and run the camera.
+  XR8.run({canvas: document.getElementById('camerafeed')})
 }
 
-function mouseMove(e){
-    mx = e.offsetX / c.width - 0.5;
-    my = 1.0 - e.offsetY / c.height;
-}
-
-function initShaders() {
-    let p = gl.createProgram();
-    let v = document.getElementById("vs").textContent;
-    let f = document.getElementById("fs").textContent;
-    let vs = gl.createShader(gl.VERTEX_SHADER);
-    let fs = gl.createShader(gl.FRAGMENT_SHADER);
-    gl.shaderSource(vs, v);
-    gl.shaderSource(fs, f);
-    gl.compileShader(vs);
-    gl.compileShader(fs);
-    console.log(gl.getShaderInfoLog(vs));
-    console.log(gl.getShaderInfoLog(fs));
-    gl.attachShader(p, vs);
-    gl.attachShader(p, fs);
-    gl.linkProgram(p);
-    gl.useProgram(p);
-    aLoc[0] = gl.getAttribLocation(p, "position");
-    aLoc[1] = gl.getAttribLocation(p, "textureCoord");
-    uLoc[0]  = gl.getUniformLocation(p, 'texture');
-    uLoc[1]  = gl.getUniformLocation(p, 'time');
-    uLoc[2]  = gl.getUniformLocation(p, 'mouse');
-    uLoc[3]  = gl.getUniformLocation(p, 'resolution');
-    gl.enableVertexAttribArray(aLoc[0]);
-    gl.enableVertexAttribArray(aLoc[1]);
-}
-
-let positionBuffer;
-let coordBuffer;
-let indexBuffer;
-let texture;
-
-function initBuffers() {
-    const positions = [
-        -1.0,  1.0,  0.0,
-         1.0,  1.0,  0.0,
-        -1.0, -1.0,  0.0,
-         1.0, -1.0,  0.0
-    ];
-    
-    const textureCoords = [
-        0.0, 0.0,
-        1.0, 0.0,
-        0.0, 1.0,
-        1.0, 1.0
-    ];
-    
-    const indices  = [
-        0, 1, 2,
-        3, 2, 1
-    ];
-    
-    positionBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
-    gl.vertexAttribPointer(aLoc[0], 3, gl.FLOAT, false, 0, 0);
-    
-    coordBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, coordBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoords), gl.STATIC_DRAW);
-    gl.vertexAttribPointer(aLoc[1], 2, gl.FLOAT, false, 0, 0);
-    
-    indexBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
-    
-    const img = new Image();
-    img.onload = function(){
-        texture = gl.createTexture();
-        gl.bindTexture(gl.TEXTURE_2D, texture);
-        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img);
-        gl.generateMipmap(gl.TEXTURE_2D);
-        animate();
-    };
-    img.src = "frog.jpeg";
-}
-
-function animate() {
-    draw();
-    requestAnimationFrame(animate);
-}
-
-let baseTime = +new Date;
-
-function draw() {
-    gl.bindTexture(gl.TEXTURE_2D, texture);
-    
-    // uniform sampler2D texture
-    gl.uniform1i(uLoc[0], 0);
-    
-    // uniform float time;
-    time = (+new Date - baseTime) / 1000;
-    gl.uniform1f(uLoc[1], time);
-    
-    // uniform vec2 mouse;
-    gl.uniform2f(uLoc[2], mx, my);
-    
-    // uniform vec2 resolution;
-    gl.uniform2f(uLoc[3], c.width, c.height);
-
-    gl.drawElements(gl.TRIANGLE_STRIP, 6, gl.UNSIGNED_SHORT, 0);
-    gl.flush();
-}
-
-initWebGL();
-initShaders();
-initBuffers();
+// Show loading screen before the full XR library has been loaded.
+const load = () => { XRExtras.Loading.showLoading({onxrloaded}) }
+window.onload = () => { window.XRExtras ? load() : window.addEventListener('xrextrasloaded', load) }
